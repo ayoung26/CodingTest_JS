bbd7ec1eb6119e48cb92b9fb1bebb26b670c3f9b
dev_a <60813174+ayoung26@users.noreply.github.com>
2024-09-04 00:10:54 +0900

    [level 2] Title: 예상 대진표, Time: 3.51 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,12 @@
+function solution(n,a,b){
+
+    let count = 0;
+    while(a !== b) {
+        count++;
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
+        console.log(`count > ${count}`)
+    }
+
+    return count;
+}
