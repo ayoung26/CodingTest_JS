aaa6cd9fc1515a897d6d4d83784e3d181ac7cfc4
dev_a <60813174+ayoung26@users.noreply.github.com>
2024-09-25 15:59:10 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 25.36 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -19,7 +19,7 @@ function solution(maps) {
     // 방문한 좌표 체크용
     let visited = new Array(m).fill(null).map(() => new Array(n).fill(false));
     
-    while(queue.length > 0) {
+    while(queue.length) {
         let [x, y, distance] = queue.shift();
         // 도착하면 반환
         if(x == n-1 && y == m-1) return distance;
@@ -29,14 +29,24 @@ function solution(maps) {
             let ny = y + direcY;
             
             // 맵을 벗어나는지 체크
-            if(nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                // 벽인지 체크하고, 방문했던 경로인지 체크
-                if(maps[ny][nx] === 1 && visited[ny][nx] === false) {
-                    visited[ny][nx] = true;
-                    // 새로운 이동경로를 큐에 추가
-                    queue.push([nx, ny, distance + 1]);
-                }
+            // if(nx >= 0 && ny >= 0 && nx < n && ny < m) {
+            //     // 벽인지 체크하고, 방문했던 경로인지 체크
+            //     if(maps[ny][nx] === 1 && visited[ny][nx] === false) {
+            //         visited[ny][nx] = true;
+            //         // 새로운 이동경로를 큐에 추가
+            //         queue.push([nx, ny, distance + 1]);
+            //     }
+            // }
+            
+            // 맵을 벗어나는지 체크
+            if(nx < 0 || ny < 0 || nx >= n || ny >= m || 
+               maps[ny][nx] == 0 || visited[ny][nx]) {
+                continue;
             }
+            
+            visited[ny][nx] = true;
+            // 새로운 이동경로를 큐에 추가
+            queue.push([nx, ny, distance + 1]);
         }
     }
     
